5a684bb8e833ff407a411ac0951f8fbb542b1bfa
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-05-08 23:56:40 -0700

    add layout property post

---
+++ docs/shiki/libs/shiki-addon.js
@@ -0,0 +1,21 @@
+remark.highlighter.engine.highlightBlock = (block, temp) => {
+   shiki_wrapper(block)
+};
+
+function shiki_wrapper (block) {
+    shiki
+      .getHighlighter({
+        theme: 'github-light'
+      })
+      .then(highlighter => {
+
+        // Add spaces to empty lines to avoid them being removed
+        const search = '<div class="remark-code-line"></div>';
+        const replaceWith = '<div class="remark-code-line"> </div>';
+        block.innerHTML = block.innerHTML.split(search).join(replaceWith);
+
+        codeLanguage = block.className.replace(" remark-code","")
+        block.innerHTML = highlighter.codeToHtml(block.innerText, codeLanguage)
+      });
+};
+
