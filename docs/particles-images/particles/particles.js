fa0530c43733652ad6a6e1468115449ae2ef2d86
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-05-09 20:39:03 -0700

    adding particles images post

---
+++ docs/particles-images/particles/particles.js
@@ -0,0 +1,163 @@
+// ParticlesJS Config.
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 55,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#fff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": [
+{"src":"PNG/RStudio.png"        ,"width": 100, "height": 100},
+{"src":"PNG/blogdown.png"       ,"width": 100, "height": 100},
+{"src":"PNG/bookdown.png"       ,"width": 100, "height": 100},
+{"src":"PNG/broom.png"          ,"width": 100, "height": 100},
+{"src":"PNG/covr.png"           ,"width": 100, "height": 100},
+{"src":"PNG/dbplot.png"         ,"width": 100, "height": 100},
+{"src":"PNG/devtools.png"       ,"width": 100, "height": 100},
+{"src":"PNG/dials.png"          ,"width": 100, "height": 100},
+{"src":"PNG/dplyr.png"          ,"width": 100, "height": 100},
+{"src":"PNG/feather.png"        ,"width": 100, "height": 100},
+{"src":"PNG/forcats.png"        ,"width": 100, "height": 100},
+{"src":"PNG/fs.png"             ,"width": 100, "height": 100},
+{"src":"PNG/ggplot2.png"        ,"width": 100, "height": 100},
+{"src":"PNG/glue.png"           ,"width": 100, "height": 100},
+{"src":"PNG/googledrive.png"    ,"width": 100, "height": 100},
+{"src":"PNG/googlesheets.png"   ,"width": 100, "height": 100},
+{"src":"PNG/haven.png"          ,"width": 100, "height": 100},
+{"src":"PNG/hms.png"            ,"width": 100, "height": 100},
+{"src":"PNG/knitr.png"          ,"width": 100, "height": 100},
+{"src":"PNG/lobstr.png"         ,"width": 100, "height": 100},
+{"src":"PNG/lubridate.png"      ,"width": 100, "height": 100},
+{"src":"PNG/modeldb.png"        ,"width": 100, "height": 100},
+{"src":"PNG/modelr.png"         ,"width": 100, "height": 100},
+{"src":"PNG/parsnip.png"        ,"width": 100, "height": 100},
+{"src":"PNG/pins.png"           ,"width": 100, "height": 100},
+{"src":"PNG/pipe.png"           ,"width": 100, "height": 100},
+{"src":"PNG/pkgdown.png"        ,"width": 100, "height": 100},
+{"src":"PNG/plumber-female.png" ,"width": 100, "height": 100},
+{"src":"PNG/purrr.png"          ,"width": 100, "height": 100},
+{"src":"PNG/r2d3.png"           ,"width": 100, "height": 100},
+{"src":"PNG/readr.png"          ,"width": 100, "height": 100},
+{"src":"PNG/readxl.png"         ,"width": 100, "height": 100},
+{"src":"PNG/recipes.png"        ,"width": 100, "height": 100},
+{"src":"PNG/reprex.png"         ,"width": 100, "height": 100},
+{"src":"PNG/rlang.png"          ,"width": 100, "height": 100},
+{"src":"PNG/rmarkdown.png"      ,"width": 100, "height": 100},
+{"src":"PNG/roxygen2.png"       ,"width": 100, "height": 100},
+{"src":"PNG/rsample.png"        ,"width": 100, "height": 100},
+{"src":"PNG/rvest.png"          ,"width": 100, "height": 100},
+{"src":"PNG/scales.png"         ,"width": 100, "height": 100},
+{"src":"PNG/shiny.png"          ,"width": 100, "height": 100},
+{"src":"PNG/sparklyr.png"       ,"width": 100, "height": 100},
+{"src":"PNG/stringr.png"        ,"width": 100, "height": 100},
+{"src":"PNG/testthat.png"       ,"width": 100, "height": 100},
+{"src":"PNG/tibble.png"         ,"width": 100, "height": 100},
+{"src":"PNG/tidymodels.png"     ,"width": 100, "height": 100},
+{"src":"PNG/tidyposterior.png"  ,"width": 100, "height": 100},
+{"src":"PNG/tidypredict.png"    ,"width": 100, "height": 100},
+{"src":"PNG/tidyr.png"          ,"width": 100, "height": 100},
+{"src":"PNG/tidyverse.png"      ,"width": 100, "height": 100},
+{"src":"PNG/usethis.png"        ,"width": 100, "height": 100},
+{"src":"PNG/vctrs.png"          ,"width": 100, "height": 100},
+{"src":"PNG/withr.png"          ,"width": 100, "height": 100},
+{"src":"PNG/xaringan.png"       ,"width": 100, "height": 100},
+{"src":"PNG/yardstick.png"      ,"width": 100, "height": 100}
+      ]
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 50,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 10,
+        "size_min": 30,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 250,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 2
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 0.5
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 4,
+        "duration": 0.3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
