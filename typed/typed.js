e9f89641fda880d6ee1db068f41ccee7e7d02e0a
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-05-11 16:14:28 -0700

    add typed post

---
+++ typed/typed.js
@@ -0,0 +1,29 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const typed = new Typed('.typed', {
+  strings: [
+    'Self typing text',
+    'Only using <b>typed.js</b>'
+  ],
+  typeSpeed: 60,
+  backSpeed: 20,
+  cursorChar: '',
+  loop: true
+});
+
+const typed2 = new Typed('.typed2', {
+  strings: [
+  "<i class='fab fa-github'/> Github_handle",
+  "<i class='fab fa-twitter'/> Twitter_handle",
+  "<i class='fab fa-linkedin'/> Linkedin_handle",
+  "<i class='fas fa-laptop'/> my-website.com",
+  ],
+  typeSpeed: 50,
+  backDelay: 2000,
+  contentType: 'html',
+  loop: true
+});
+</script>
+
+
